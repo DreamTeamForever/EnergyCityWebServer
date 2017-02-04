eece740571985058aadeea70a337777d1377eb0f
IgorPolyakov <igorpolyakov@protonmail.com>
2017-02-04 18:54:42 +0700

    refactoring

--- morris-data.js
+++ morris-data.js
@@ -6,62 +6,65 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '2015 Q1',
+            gas: 2666,
+            heat: null,
+            energy: 2647
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2015 Q2',
+            gas: 2778,
+            heat: 2294,
+            energy: 2441
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2015 Q3',
+            gas: 4912,
+            heat: 1969,
+            energy: 2501
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '2015 Q4',
+            gas: 3767,
+            heat: 3597,
+            energy: 5689
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '2016 Q1',
+            gas: 6810,
+            heat: 1914,
+            energy: 2293
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '2016 Q2',
+            gas: 5670,
+            heat: 4293,
+            energy: 1881
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '2016 Q3',
+            gas: 4820,
+            heat: 3795,
+            energy: 1588
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '2016 Q4',
+            gas: 15073,
+            heat: 5967,
+            energy: 5175
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '2017 Q1',
+            gas: 10687,
+            heat: 4460,
+            energy: 2028
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '2017 Q2',
+            gas: 8432,
+            heat: 5713,
+            energy: 1791
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
-        pointSize: 2,
+        ykeys: ['gas', 'heat', 'energy'],
+        labels: ['Газ', 'Тепло', 'Электричество'],
+        pointSize: 4,
         hideHover: 'auto',
-        resize: true
+        resize: true,
+        lineColors: ['#87d6c6', '#54cdb4','#1ab394'],
+        lineWidth:2,
+        pointSize:1,
     });
 
     // Donut Chart
@@ -87,97 +90,97 @@ $(function() {
         // Chart data records -- each entry in this array corresponds to a point on
         // the chart.
         data: [{
-            d: '2012-10-01',
+            d: '2017-10-01',
             visits: 802
         }, {
-            d: '2012-10-02',
+            d: '2017-10-02',
             visits: 783
         }, {
-            d: '2012-10-03',
+            d: '2017-10-03',
             visits: 820
         }, {
-            d: '2012-10-04',
+            d: '2017-10-04',
             visits: 839
         }, {
-            d: '2012-10-05',
+            d: '2017-10-05',
             visits: 792
         }, {
-            d: '2012-10-06',
+            d: '2017-10-06',
             visits: 859
         }, {
-            d: '2012-10-07',
+            d: '2017-10-07',
             visits: 790
         }, {
-            d: '2012-10-08',
+            d: '2017-10-08',
             visits: 1680
         }, {
-            d: '2012-10-09',
+            d: '2017-10-09',
             visits: 1592
         }, {
-            d: '2012-10-10',
+            d: '2017-10-10',
             visits: 1420
         }, {
-            d: '2012-10-11',
+            d: '2017-10-11',
             visits: 882
         }, {
-            d: '2012-10-12',
+            d: '2017-10-12',
             visits: 889
         }, {
-            d: '2012-10-13',
+            d: '2017-10-13',
             visits: 819
         }, {
-            d: '2012-10-14',
+            d: '2017-10-14',
             visits: 849
         }, {
-            d: '2012-10-15',
+            d: '2017-10-15',
             visits: 870
         }, {
-            d: '2012-10-16',
+            d: '2017-10-16',
             visits: 1063
         }, {
-            d: '2012-10-17',
+            d: '2017-10-17',
             visits: 1192
         }, {
-            d: '2012-10-18',
+            d: '2017-10-18',
             visits: 1224
         }, {
-            d: '2012-10-19',
+            d: '2017-10-19',
             visits: 1329
         }, {
-            d: '2012-10-20',
+            d: '2017-10-20',
             visits: 1329
         }, {
-            d: '2012-10-21',
+            d: '2017-10-21',
             visits: 1239
         }, {
-            d: '2012-10-22',
+            d: '2017-10-22',
             visits: 1190
         }, {
-            d: '2012-10-23',
+            d: '2017-10-23',
             visits: 1312
         }, {
-            d: '2012-10-24',
+            d: '2017-10-24',
             visits: 1293
         }, {
-            d: '2012-10-25',
+            d: '2017-10-25',
             visits: 1283
         }, {
-            d: '2012-10-26',
+            d: '2017-10-26',
             visits: 1248
         }, {
-            d: '2012-10-27',
+            d: '2017-10-27',
             visits: 1323
         }, {
-            d: '2012-10-28',
+            d: '2017-10-28',
             visits: 1390
         }, {
-            d: '2012-10-29',
+            d: '2017-10-29',
             visits: 1420
         }, {
-            d: '2012-10-30',
+            d: '2017-10-30',
             visits: 1529
         }, {
-            d: '2012-10-31',
+            d: '2017-10-31',
             visits: 1892
         }, ],
         // The name of the data record attribute that contains x-visitss.
@@ -196,22 +199,22 @@ $(function() {
     Morris.Bar({
         element: 'morris-bar-chart',
         data: [{
-            device: 'iPhone',
+            device: 'gas',
             geekbench: 136
         }, {
-            device: 'iPhone 3G',
+            device: 'gas 3G',
             geekbench: 137
         }, {
-            device: 'iPhone 3GS',
+            device: 'gas 3GS',
             geekbench: 275
         }, {
-            device: 'iPhone 4',
+            device: 'gas 4',
             geekbench: 380
         }, {
-            device: 'iPhone 4S',
+            device: 'gas 4S',
             geekbench: 655
         }, {
-            device: 'iPhone 5',
+            device: 'gas 5',
             geekbench: 1571
         }],
         xkey: 'device',
@@ -225,3 +228,4 @@ $(function() {
 
 
 });
+
