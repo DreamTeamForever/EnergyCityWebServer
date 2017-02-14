b9984ff83981ae38763bfa28bbd60615e55e3891
neverhooda <neverhooda@gmail.com>
2017-02-14 11:03:17 +0700

    add edit table model

--- public/js/modalWindow.js
+++ public/js/modalWindow.js
@@ -2,6 +2,9 @@ var sun_select    = document.getElementById("input_sol_model");
 var wind_select   = document.getElementById("input_wind_model");
 var save_settings = document.getElementById("btn_save_settings");
 var game_speed    = document.getElementById("input_speed");
+var table_model;
+edit_table_model();
+
 var model_request = new XMLHttpRequest();
 model_request.open('GET', 'http://localhost/data/default/table_models.json');
 model_request.onload = function() {
@@ -29,6 +32,7 @@ save_settings.addEventListener("click", function() {
 
 $(document).ready(function() {
   updateModalSettings();
+  
   // testSocet();
 });
 
@@ -100,31 +104,86 @@ function eraseCookie(name) {
 // }
 // xmlHttp.send(postData);
 // };
+//Херня для таблицы 0
+//http://www.json-generator.com/api/json/get/cgvGBbDrJu?indent=2
+
+function edit_table_model() {
+  var table_model_r = new XMLHttpRequest();
+  table_model_r.open('GET', 'http://www.json-generator.com/api/json/get/cgvGBbDrJu?indent=2');
+  table_model_r.onload = function() {
+    if (table_model_r.status >= 200 && table_model_r.status < 400) {
+      table_model = JSON.parse(table_model_r.responseText);
+      renderModel(table_model);
+    } else {
+      console.log("Server error code: "+table_model_r.status);
+    }
+  };
+  table_model_r.send();
+}
+
+function renderModel(data) {
+  var input_model = document.getElementById("select_model");
+  for (var i = 0; i < data.length; i++) {
+    var option = document.createElement("option");
+    option.text = data[i].model_name;
+    input_model.add(option, input_model[i]); 
+  }
+
+
+  // var x = document.createElement("TR");
+  //   x.setAttribute("id", "myTr");
+  //   document.getElementById("myTable").appendChild(x);
+
+  //   var y = document.createElement("TD");
+  //   var t = document.createTextNode("new cell");
+  //   y.appendChild(t);
+  //   document.getElementById("myTr").appendChild(y);
+}
+
+//do not fuck, how it works
+function select_on_model() {
+  $("#table_model_body tr").remove(); 
+  var model_n = document.getElementById("select_model").value;
+  for(var i = 0; i < table_model.length; i++) {
+    if(model_n==table_model[i].model_name){
+      for(var j = 0; j < table_model[i].data.length; j++ ) {
+        var table_c = document.getElementById('table_current_model').getElementsByTagName('tbody')[0];
+        var new_row   = table_c.insertRow(table_c.rows.length);
+        var new_cell  = new_row.appendChild(document.createElement("TH")).innerText = table_model[i].data[j].id ;
+        var new_cell_2  = new_row.insertCell(1).innerText = table_model[i].data[j].value;
+      }
+    }
+  }
+  event_edit_table();
+}
+
 
 //Херня для таблицы I
-$(function() { 
-  $('td').click(function(e) { 
-    var t = e.target || e.srcElement; 
-    var elm_name = t.tagName.toLowerCase(); 
-    if(elm_name == 'input') {
-      return false;
-    }
-    var val = $(this).html(); 
-    var code = '<input type="text" id="edit" value="'+val+'" />'; 
-    $(this).empty().append(code); 
-    $('#edit').focus(); 
-    $('#edit').blur(function() { 
-      var val = $(this).val(); 
-      $(this).parent().empty().html(val); 
+function event_edit_table() {
+  $(function() {
+    $('td').click(function(e) { 
+      var t = e.target || e.srcElement; 
+      var elm_name = t.tagName.toLowerCase(); 
+      if(elm_name == 'input') {
+        return false;
+      }
+      var val = $(this).html(); 
+      var code = '<input type="text" id="edit" value="'+val+'" />'; 
+      $(this).empty().append(code); 
+      $('#edit').focus(); 
+      $('#edit').blur(function() { 
+        var val = $(this).val(); 
+        $(this).parent().empty().html(val); 
+      }); 
     }); 
-  }); 
-});
-//Херня для таблицы II + Enter
-$(window).keydown(function(event){ 
-  if(event.keyCode == 13) {
-    $('#edit').blur();
-  } 
-});
+  });
+  //Херня для таблицы II + Enter
+  $(window).keydown(function(event){ 
+    if(event.keyCode == 13) {
+      $('#edit').blur();
+    } 
+  });
+}
 
 //магия бляяяять, Херня для таблицы III
 //$('#odin').editable();
