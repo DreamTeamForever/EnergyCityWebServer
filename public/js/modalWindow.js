69a4c726b5d6c05f6be421ac93fe4b907e82d17d
neverhooda <neverhooda@gmail.com>
2017-02-22 16:02:22 +0700

    ref code js && add func for form settings

--- public/js/modalWindow.js
+++ public/js/modalWindow.js
@@ -1,181 +1,89 @@
+//----Было надо, но теперь. Пусть будет----
 var sun_select    = document.getElementById("input_sol_model");
 var wind_select   = document.getElementById("input_wind_model");
 var save_settings = document.getElementById("btn_save_settings");
 var game_speed    = document.getElementById("input_speed");
-var table_model;
+//-----------------------------------------
 
+//----Глобальные переменные и все такое----
+var cs_url        = "http://82.117.171.124:9099/"; // Адресс сервера
+var table_model;  //Данные описывающие все модели
+var game_model;   //Данные описывающие игровую сессию
+var object_model; //Данные описывающие объекты и их настройки
+//-----------------------------------------
 
-// var model_request = new XMLHttpRequest();
-// model_request.open('GET', 'http://localhost/data/default/table_models.json');
-// model_request.onload = function() {
-  
-//   if (model_request.status >= 200 && model_request.status < 400) {
-//     var model_select = JSON.parse(model_request.responseText);
-//     renderHTML(model_select);
-//   } else {
-//     console.log("Server error code: "+model_request.status);
-//   }
-// };
-
-// model_request.onerror = function() {
-//   console.log("Connection error");
-// };
-
-// model_request.send();
-
-save_settings.addEventListener("click", function() {
-  writeCookie("game_speed",game_speed.value,10);
-  writeCookie("sun_select",sun_select.value,10);
-  writeCookie("wind_select",wind_select.value,10);
-  $('#game_settings').modal('hide');
-});
-
+//----Отработка ф-ци по завршению загрузки страницы----
 $(document).ready(function() {
-  updateModalSettings();
-  load_model();
-  // edit_table_model();
-  //   // testSocet();
-  //    testSocet_2();
+  loadData("modelCollections",loadModel);
+  loadData("gameSettings",lodaGameSettings);
+  //updateModalSettings();
+  //load_model();
 });
+//-----------------------------------------
 
-function updateModalSettings(){
-  $('#game_settings').on('show.bs.modal', function() {
-      $("#input_sol_model").val(readCookie ("sun_select"));
-      $("#input_wind_model").val(readCookie ("wind_select"));
-      $("#input_speed").val(readCookie ("game_speed"));
-  })
+//----Изменение состояния кнопки(вкл/выкл)----
+// isActive - активна ли кнопка true/false
+// btn_id   - идентефикатор кнопки
+function changeStateButtom(isActive, btn_id) {
+  if(isActive){
+    document.getElementById(btn_id).removeAttribute("disabled"); 
+  } else {
+    document.getElementById(btn_id).setAttribute("disabled","disabled");
+  }
 }
+//-----------------------------------------
 
-function renderHTML(data) {
-  $("#input_sol_model option").remove();
-  $("#input_wind_model option").remove();
-  for (var i = 0; i < data.length; i++) {
-    var option = document.createElement("option");
-    option.text = data[i].model_name;
-    if(data[i].model_type == "sol_model") {
-      sun_select.add(option, sun_select[i]);
-    } else if (data[i].model_type == "wind_model") {
-      wind_select.add(option, wind_select[i]);
-    }
-  }
-};
-function writeCookie(name, value, days) {
-  var expires = "";
-  if(days) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days*24*60*60*1000));
-    expires = "; expires=" + date.toGMTString();
-    document.cookie = name + "=" + value + expires + "; path=/";
-  }
-};
-
-function readCookie(name) {
-  var searchName = name + "=";
-  var cookies = document.cookie.split(';');
-  for (var i = 0; i < cookies.length; i++) {
-    var c = cookies[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1, c.length);
-    }
-    if (c.indexOf(searchName) == 0)
-      return c.substring(searchName.length, c.length);
-  }
-  return null;
-};
-
-function eraseCookie(name) {
-  writeCookie(name, "", -1);
-};
-
-function testSocet() {
-
-var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open('POST','http://82.117.171.124:9099/test100500',false);
-    // xmlHttp.onload = function () {
-    //  var status = request.status; 
-    //  var data = request.responseText; 
-    // }
-    //setRequestHeader("Content-type", "application/x-www-form-urlencoded"
-    xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-    var postData = "1111111Some data dfgdfgdfgdfgdf gdfgsfsdgm233423423423423dfgdfgsdfg7657ghjghjfgjhfgj sdfgsdfgsdfgsdfgdsfg";
-    // console.log("data: "+postData);
-    // xmlHttp.send(postData);
-    // console.log("Test: "+xmlHttp.status);
-    xmlHttp.onreadystatechange = function() {
-    if(xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        console.log(xmlHttp.responseText);
-    }
-}
-xmlHttp.send(postData);
-};
-
-function testSocet_2() {
-var xmlHttp = new XMLHttpRequest();
-var test;
-    xmlHttp.open('GET','http://82.117.171.124:9099/modelCollections');
-    xmlHttp.onload = function() {
-      if (xmlHttp.status >= 200 && xmlHttp.status < 400) {
-        //console.log("Server error code: "+xmlHttp.responseText);
-        test = JSON.parse(xmlHttp.responseText);
-        console.log("Server code: "+test);
-        //renderHTML(model_select);
-      } else {
-        console.log("Server error code: "+xmlHttp.status);
-      }
-    }
-    xmlHttp.send();
-};
-
-
-
-//Херня для таблицы 0
-//http://www.json-generator.com/api/json/get/cgvGBbDrJu?indent=2
-
-//Ну и гавно коня
-function load_model() {
+//----Загрузка данных с сервера------------
+// url  - путь до файла на сервере
+// callback - ну он и в африке callback
+function loadData(url,callback) {
   var xmlHttp = new XMLHttpRequest();
-  var test;
-    xmlHttp.open('GET','http://82.117.171.124:9099/modelCollections');
-    xmlHttp.onload = function() {
-      if (xmlHttp.status >= 200 && xmlHttp.status < 400) {
-        table_model = JSON.parse(xmlHttp.responseText);
-        renderModel(table_model);
-        renderHTML(table_model);
-      } else {
-        console.log("Server error code: "+xmlHttp.status);
-      }
+  xmlHttp.open('GET',cs_url+url);
+  xmlHttp.onload = function() {
+    if (xmlHttp.status >= 200 && xmlHttp.status < 400) {
+      var data = JSON.parse(xmlHttp.responseText);
+      callback(data);        
+    } else {
+      console.log("Server error code: "+xmlHttp.status);
     }
-    xmlHttp.send();
+  }
+  xmlHttp.send();
 };
+//-----------------------------------------
 
-function save_model(data) {
+//----Сохранение данных на сервер----------
+// url  - путь до файла на сервере
+// data - данные в формате Json
+function saveData(url,data) {
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open('POST','http://82.117.171.124:9099/modelCollections');
+  xmlHttp.open('POST',cs_url+url);
   xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
   xmlHttp.onreadystatechange = function() {
     if(xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      console.log(xmlHttp.responseText);
+      console.log("Server say 200: "+xmlHttp.responseText);
     }
   }
   xmlHttp.send(data);
 };
+//-----------------------------------------
 
-
-function edit_table_model() {
-  var table_model_r = new XMLHttpRequest();
-  table_model_r.open('GET', 'http://www.json-generator.com/api/json/get/cgvGBbDrJu?indent=2');
-  table_model_r.onload = function() {
-    if (table_model_r.status >= 200 && table_model_r.status < 400) {
-      table_model = JSON.parse(table_model_r.responseText);
-      renderModel(table_model);
-    } else {
-      console.log("Server error code: "+table_model_r.status);
-    }
-  };
-  table_model_r.send();
+//----НАСТРОЙКИ ТАБЛИЦ МОДЕЛЕЙ-----------
+//----Загрузка данных о моделях--------
+function loadModel(data) {
+  table_model = data;
 }
+//-----------------------------------------
 
-function renderModel(data) {
+//----Обновление формы "Выбор модели" при открытии-----------
+$('#model_settings').on('show.bs.modal', function() {
+  updateSelectModel(table_model);
+  $("#table_model_body tr").remove();
+});
+//-----------------------------------------
+
+//----Обновление селектора-----------
+function updateSelectModel(data) {
+  $("#select_model option").remove();
   var input_model = document.getElementById("select_model");
   for (var i = 0; i < data.length; i++) {
     var option = document.createElement("option");
@@ -183,9 +91,10 @@ function renderModel(data) {
     input_model.add(option, input_model[i]); 
   }
 }
+//-----------------------------------------
 
-//do not fuck, how it works
-function select_on_model() {
+//----Отображение таблицы при выборе модели--------
+function selectModel() {
   changeStateButtom(true,"btn_edit_model");
   changeStateButtom(true,"btn_delet_model");
   $("#table_model_body tr").remove(); 
@@ -202,10 +111,10 @@ function select_on_model() {
     }
   }
 }
+//-----------------------------------------
 
-
-//Херня для таблицы I
-function event_edit_table() {
+//----Редактирование модели по кнопке--------
+function editTable() {
   changeStateButtom(false,"btn_edit_model");
   $(function() {
     $('td').click(function(e) { 
@@ -224,25 +133,32 @@ function event_edit_table() {
       }); 
     }); 
   });
-
-  //Херня для таблицы II + Enter
-  $(window).keydown(function(event){ 
+  $(window).keydown(function(event){  //Херня для таблицы II + Enter
     if(event.keyCode == 13) {
       $('#edit').blur();
     } 
   });
 }
+//-----------------------------------------
 
- //Херня для кнопки I - balalayka
-function changeStateButtom(isActive, btn_id) {
-  if(isActive){
-    document.getElementById(btn_id).removeAttribute("disabled"); 
-  } else {
-    document.getElementById(btn_id).setAttribute("disabled","disabled");
+//----Удаление модели по кнопке--------
+function deletTableModel() {
+  changeStateButtom(false,"btn_delet_model");
+  changeStateButtom(false,"btn_edit_model");
+  var name = $("#select_model").val();
+  for (var i = 0; i < table_model.length; i++) {
+    if(name == table_model[i].model_name){
+      table_model.splice(i,1);
+      break;
+    }
   }
+  $("#table_model_body tr").remove();
+  updateSelectModel(table_model); 
 }
+//-----------------------------------------
 
-function create_table_model() {
+//----Создание и редактирование таблицы моделей--------
+function createTableModel() {
   var new_name = $("#create_model").val();
   for (var i =0; i < table_model.length; i++) {
     if(table_model[i].model_name == new_name){
@@ -253,11 +169,11 @@ function create_table_model() {
   $("#form_create_model").attr("class","form-group has-success");
   changeStateButtom(false,"btn_create_model");
   changeStateButtom(true,"btn_add_model");
-  create_table(new_name);
+  initTable(new_name);
   console.log("test");
 }
 
-function create_table(n_m) {
+function initTable(n_m) {
   $("#table_model_body tr").remove(); 
   for(var j = 0; j < 24; j++ ) {
     var table_c   = document.getElementById('table_current_model').getElementsByTagName('tbody')[0];
@@ -265,23 +181,23 @@ function create_table(n_m) {
     new_row.appendChild(document.createElement("TH")).innerText = j ;
     new_row.insertCell(1).innerText = 0;
   }
-  event_edit_table();
-
+  editTable();
 }
+//-----------------------------------------
 
-function add_table_model(){
+//----Добавлние таблицы модели в коллекцию--------
+function addTableModel(){
   changeStateButtom(true,"btn_create_model");
   changeStateButtom(false,"btn_add_model");
   $("#form_create_model").attr("class","form-group");
   var new_name = $("#create_model").val();
-  add_model_object(new_name);
+  addModelObject(new_name);
   $("#select_model option").remove();
-  renderModel(table_model);
+  updateSelectModel(table_model); //#
   $("#create_model").val("");
 }
 
-
-function add_model_object(newObject) {
+function addModelObject(newObject) {
   var model_object = {
     model_id: "",
     model_name: "",
@@ -291,29 +207,11 @@ function add_model_object(newObject) {
   model_object.model_id = newObject;
   model_object.model_name = newObject;
   model_object.model_type = "vse_huina"
-  model_object.data = get_table_data($("#table_current_model  > tbody"));
+  model_object.data = getTableData($("#table_current_model  > tbody"));
   table_model.push(model_object);
-  // for (var i = 0; i < table_model.length; i++) {
-  //   console.log(table_model[i].data)
-  // }
 }
 
-function delet_table_model() {
-  changeStateButtom(false,"btn_delet_model");
-  changeStateButtom(false,"btn_edit_model");
-  var name = $("#select_model").val();
-  for (var i = 0; i < table_model.length; i++) {
-    if(name == table_model[i].model_name){
-      table_model.splice(i,1);
-      break;
-    }
-  }
-  $("#select_model option").remove();
-  $("#table_model_body tr").remove();
-  renderModel(table_model);
-}
-
-function get_table_data(table) {
+function getTableData(table) {
     var data = [];
     table.find('tr').each(function (rowIndex, r) {
         var data_model = {
@@ -328,23 +226,79 @@ function get_table_data(table) {
           }           
         });
         data.push(data_model);
-        //console.log(data_model);
     });
-    //console.log(data);
     return data;
 }
+//-----------------------------------------
 
-function save_changes_model() {
-  save_model(JSON.stringify(table_model));
+//----Сохранение коллекции моделей на сервере--------
+function saveСhangesModel() {
+  saveData("modelCollections",JSON.stringify(table_model)); 
 }
+//-----------------------------------------
 
-function discard_changes_model() {
-  $("#select_model option").remove();
+//----Отмена внесенных изменений--------
+function discardChangesModel() {
   $("#table_model_body tr").remove();
-  load_model();
+  loadData("modelCollections",loadModel);
+  loadData("modelCollections",updateSelectModel);
 }
+//-----------------------------------------
+//-----------------------------------------
 
+//----ИГРОВЫЕ НАСТРОЙКИ-----------
+//----Кэширование данных--------
+function lodaGameSettings(data){
+  game_model = data;
+}
+//-----------------------------------------
 
+//----Обновление формы "Настройки игры" при открытии-------
+$('#game_settings').on('show.bs.modal', function() {
+  updateFormSettings(game_model);
+});
+//-----------------------------------------
 
-//магия бляяяять, Херня для таблицы III
-//$('#odin').editable();
+//----Обновление полей формы--------
+function updateFormSettings(data) {
+  updateSelectSettings(table_model);
+  $("#inputSolModel").val(data.sunModel);
+  $("#inputWindModel").val(data.windModel);
+  $("#inputSpeed").val(data.gameSpeed);
+}
+//-----------------------------------------
+
+//----Обновление селекторов--------
+function updateSelectSettings(data) {
+  $("#inputSolModel option").remove();
+  $("#inputWindModel option").remove();
+  var sun_select = document.getElementById("inputSolModel");
+  var wind_select = document.getElementById("inputWindModel");
+  for (var i = 0; i < data.length; i++) {
+    var option = document.createElement("option");
+    option.text = data[i].model_name;
+    if(data[i].model_type == "sol_model") {
+      sun_select.add(option, sun_select[i]);
+    } else if (data[i].model_type == "wind_model") {
+      wind_select.add(option, wind_select[i]);
+    }
+  }
+}
+//-----------------------------------------
+
+//----Сохранение настроек на сервере--------
+function saveGameSettings() {
+  game_model.gameSpeed = $("#inputSpeed").val();
+  game_model.sunModel  = $("#inputSolModel").val();
+  game_model.windModel = $("#inputWindModel").val();
+  saveData("gameSettings",JSON.stringify(game_model));
+}
+//-----------------------------------------
+
+//----Отмена изменений--------
+function discardChangesSettings() {
+  updateFormSettings(game_model);
+}
+//-----------------------------------------
+//-----------------------------------------
+
