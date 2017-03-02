c6abf8e00b288b97f2b6a4b0d4e8b3e9ce51f6e7
neverhooda <neverhooda@gmail.com>
2017-03-02 17:30:17 +0700

    dell styleSettings

--- public/js/settingsGraph.js
+++ public/js/settingsGraph.js
@@ -1,7 +1,6 @@
 //----Глобальные переменные и все такое---
 var graphData; //данные для графа
 
-
 function getSettingsGraph() {
 	var settignsGraph = {
 	container: document.getElementById('cy'),
@@ -103,76 +102,5 @@ function getDataGraph() {
 	}
 
 	return collD;
-}
-//-----------------------------------------
-function getStyleGraph() {
-	var stg = [{
-                selector: 'node',
-                style: { 'width': 60, 'height': 60, 'content': 'data(label)', 'text-valign': 'top', 'color': '#1ab394', 'background-fit': 'cover', 'border-color': '#000', 'border-width': 5, 'border-opacity': 0.5 }
-            },
-            {
-                selector: 'edge',
-                style: { 'width': 3, 'target-arrow-shape': 'triangle', 'line-color': 'data(color)', 'target-arrow-color': 'data(color)' }
-            },
-            {
-                selector: ':selected',
-                style: { 'background-color': 'yellow', 'line-color': 'yellow', 'target-arrow-color': 'black', 'source-arrow-color': 'black' }
-            },
-            {
-                selector: 'edge:selected',
-                style: { 'width': 20 }
-            },
-
-            {
-                selector: 'node[type = "stick"]',
-                style: { 'width': 6, 'height': 6 }
-            },
-            {
-                selector: 'node[type = "factory"]',
-                style: { 'background-image': 'image/factory.jpg'}
-            },
-            {
-                selector: 'node[type = "accumulate"]',
-                style: { 'background-image': 'image/accamulator.jpg' }
-            },
-            {
-                selector: 'node[type = "hospital"]',
-                style: {'background-image': 'image/hospital.jpg' }
-            },
-            {
-                selector: 'node[type = "district"]',
-                style: { 'background-image': 'image/district.jpg' }
-            },
-            {
-                selector: 'node[type = "disel"]',
-                style: { 'background-image': 'image/diesel.jpg' }
-            },
-            {
-                selector: 'node[type = "wind"]',
-                style: { 'background-image': 'image/wind.jpg' }
-            },
-            {
-                selector: 'node[type = "sun"]',
-                style: { 'background-image': 'image/solar.jpg' }
-            },
-            {
-                selector: 'node[type = "electric_substaion"]',
-                style: { 'background-image': 'image/electric.jpg' }
-            },
-            {
-                selector: 'node[type = "mini_electric_substaion"]',
-                style: { 'background-image': 'image/electric.jpg' }
-            },
-            {
-                selector: 'node[type = "stick"]',
-                style: { 'background-image': 'image/stcik.png' }
-            },
-            {
-                selector: '#electric_substaion',
-                style: { 'background-image': 'image/electric.jpg' }
-            }
-        ];
-
-	return stg;
 }
 //-----------------------------------------
