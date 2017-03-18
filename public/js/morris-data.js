777e0e5d7cdd4bad69ebf96530066e30d64b4b2c
IgorPolyakov <igorpolyakov@protonmail.com>
2017-03-18 15:08:58 +0700

    add economic modal window

--- public/js/morris-data.js
+++ public/js/morris-data.js
@@ -22,37 +22,44 @@ var chartMoris = Morris.Area({
     lineColors: ['green', 'red']
 });
 //-----------------------------------------
-
-//-------СЕКТОР ПРИЗ-----------------------
-Morris.Donut({
-    element: 'morris-donut-sun',
-    data: [{
-        label: "Ligth",
-        value: 12
-    }, {
-        label: "Dark",
-        value: 45
-    }],
-    resize: true
-});
-
-Morris.Donut({
-    element: 'morris-donut-wind',
-    data: [{
-        label: "Power",
-        value: 12
-    }, {
-        label: "Brizz",
-        value: 30
-    }],
+var chartMoris = Morris.Area({
+    element: 'area-chart-econom',
+    data: data,
+    xkey: ['step'],
+    ykeys: ['input', 'output'],
+    labels: ['Total Income', 'Total Outcome'],
+    fillOpacity: 0.6,
+    hideHover: 'auto',
+    behaveLikeLine: true,
     resize: true,
-    colors: [
-        '#0BA462',
-        '#39B580',
-        '#67C69D',
-        '#95D7BB'
-    ],
+    parseTime: false,
+    pointFillColors: ['#ffffff'],
+    pointStrokeColors: ['blue'],
+    lineColors: ['green', 'red']
 });
+//-------СЕКТОР ПРИЗ-----------------------
+var data = [{
+        y: '2014',
+        a: 50,
+        b: 90
+    },
+    {
+        y: '2015',
+        a: 65,
+        b: 75
+    },
+    {
+        y: '2016',
+        a: 50,
+        b: 50
+    },
+    {
+        y: '2024',
+        a: 160,
+        b: 95
+    }
+];
+
 //-----------------------------------------
 
 //----загрузка данных----------------------
@@ -66,6 +73,35 @@ function loadChart(data) {
 function updateChart() {
     loadData("dataChart", loadChart);
 }
+//-----------------------------------------
+
+//----обновление данных по таймеру---------
+function enableTimerCh(gameState, time) {
+    if (gameState) {
+        chartTimer = setInterval('updateChart()', time);
+    } else {
+        clearInterval(chartTimer);
+    }
+}
+
+function setIdObjectChart(id) {
+    console.log(id);
+    idObject = id;
+    updateChart();
+}
+
+function findObject(data) {
+    var temp = data[0].object_data;
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].object_id == idObject) {
+            temp = data[i].object_data;
+            break;
+        }
+    }
+    return temp;
+}
+//-----------------------------------------
+
 //-----------------------------------------
 
 //----обновление данных по таймеру---------
